7015c42f4c1fab2660028a826ec13c458c40b3e9
ghoppen <gertautas1@gmail.com>
2022-04-02 16:04:58 +0200

    added number types

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("something");
+function add(n1, n2) {
+    console.log(typeof number2);
+    return n1 + n2;
+}
+var number1 = 5;
+var number2 = 2.8;
+var result = add(number1, number2);
+console.log(result);
